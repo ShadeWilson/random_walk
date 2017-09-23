4db0a1270240e4e1996db8be1870f3079b50ed7f
Shade Wilson <shadew@uw.edu>
2017-09-23 15:06:20 -0700

    Add optional arguments for color gradient and step size

--- random_walk.R
+++ random_walk.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 
-random_walk <- function(time = 10000) {
+random_walk <- function(time = 10000, 
+                        gradient = c("red","yellow","green", "lightblue","darkblue"),
+                        step_size = 1) {
+  
+  stopifnot(is.numeric(time), is.character(gradient), is.numeric(step_size))
+  
   walk <- (1:time)
   
   x <- vector("integer", length = length(walk))
@@ -13,18 +18,20 @@ random_walk <- function(time = 10000) {
            dy = cos(random))
   
   for (i in seq_along(1:(time-1))) {
-    walk$x[i + 1] <- walk$x[i] + walk$dx[i + 1]
-    walk$y[i + 1] <- walk$y[i] + walk$dy[i + 1]
+    walk$x[i + 1] <- walk$x[i] + walk$dx[i + 1] * step_size
+    walk$y[i + 1] <- walk$y[i] + walk$dy[i + 1] * step_size
   }
   
   plot <- ggplot(walk, aes(x, y, color = walk)) +
     geom_path() + 
     labs(color = "Time") +
-    scale_colour_gradientn(colors = c("red","yellow","green","lightblue","darkblue")) +
+    scale_colour_gradientn(colors = gradient) +
     theme_minimal()
   
   print(plot)
 }
 
+
 random_walk()
+
   
